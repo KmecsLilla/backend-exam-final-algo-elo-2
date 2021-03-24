30ed08e671d8645c0675dda345d25606d246704e
KmecsLilla <kmecs.lilla@gmail.com>
2021-03-24 15:24:08 +0100

    Implement the algo-task

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,13 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+
+        int accumulator = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (Math.abs(numbers.get(i) % 2) == 1) {
+                accumulator += numbers.get(i);
+            }
+        }
+        return accumulator;
     }
 }
